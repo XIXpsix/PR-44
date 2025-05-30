256846e88374834a6438319cf985e896730fe7f3
Kab-35-16 <Kab-35-16@KAB-31-7->
2025-05-30 12:10:40 +0300

    .

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_44
+{
+    class Person <T>
+    {
+        public T Id { get; }
+        public string Name { get; }
+        public Person() { }
+        public Person (T id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace PR_44
     {
         static void Main(string[] args)
         {
+            //Person <int> tom = new Person <int>(546, "Tom");
+            //Person <string> bob = new Person <string>("a1", "Bob");
+            //int TomId = (int)tom.Id;
+            //string BobId = (string)bob.Id;
+            //Console.WriteLine(TomId);
+            //Console.WriteLine(BobId);
+
+            Person<int> tom = new Person<int>(546, "Tom");
+            Company<Person<int>> microsoft = new Company<Person<int>>(tom);
+            Console.WriteLine(microsoft.CEO.Id);
+            Console.WriteLine(microsoft.CEO.Name);
         }
     }
 }
