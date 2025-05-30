4855efba029b4724b8a58adf76c9979b6773c298
Kab-35-16 <Kab-35-16@KAB-31-7->
2025-05-30 12:48:28 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,15 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace PR_44
+namespace GenericPointApp
 {
+    // Обобщённый класс Point
+    public class Point<T>
+    {
+        // Внутренние поля
+        private T x;
+        private T y;
+
+        // Конструктор с двумя параметрами
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Свойства доступа к внутренним полям
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        // Метод вывода значений полей
+        public void Print()
+        {
+            Console.WriteLine($"Point coordinates: X = {x}, Y = {y}");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            // Точка с int
+            Point<int> intPoint = new Point<int>(10, 20);
+            intPoint.Print();
+
+            // Точка с uint
+            Point<uint> uintPoint = new Point<uint>(15, 25);
+            uintPoint.Print();
+
+            // Точка с double
+            Point<double> doublePoint = new Point<double>(12.5, 30.7);
+            doublePoint.Print();
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
         }
     }
 }
