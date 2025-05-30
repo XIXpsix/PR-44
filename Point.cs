8f5442472e69b52431d4f7dd6996b4c6678701a6
Kab-35-16 <Kab-35-16@KAB-31-7->
2025-05-30 12:31:37 +0300

    .

---
+++ Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_44
+{
+    class Point<T>
+    {
+        private T x;
+        private T y;
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Координаты x= {x};");
+            Console.WriteLine($"Координаты y= {y};");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace PR_44
     {
         static void Main(string[] args)
         {
+            Point<int> P1 = new Point<int>(10, 20);
+            P1.Print();
+            Point<uint> P2 = new Point<uint>(110, 400);
+            P2.Print();
+            Point<double> P3 = new Point<double>(110, 400);
+            P3.Print();
+            Console.ReadKey();
         }
     }
 }
